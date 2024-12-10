ff661fc3beffab0b990e3bf869035e68c0fa8679
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2024-12-10 22:49:18 +0100

    init react boilerplate with vite

---
+++ todo/frontend/react-vite-app/src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import './App.css';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>
+        Vite + React4 <span className="text-blue-500 m-10">4</span>
+      </h1>
+      <div>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <div className="flex justify-center items-center h-screen bg-blue-500 text-white text-3xl">Tailwind CSS with React and Vite!</div>
+    </>
+  );
+}
+
+export default App;
---
+++ todo/frontend/react-vite-app/vite.config.ts
@@ -0,0 +1,12 @@
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+});
