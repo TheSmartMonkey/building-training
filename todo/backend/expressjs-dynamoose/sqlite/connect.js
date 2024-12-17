ef753c3f54c096dff4b4976dbabe2a69ab6512fe
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2024-12-17 19:04:01 +0100

    cleanup and create dynamodb table

--- todo/backend/expressjs-dynamoose/sqlite/connect.js
+++
@@ -1,9 +0,0 @@
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
-
-export async function connectToDatabase() {
-  return open({
-    filename: './db.sqlite',
-    driver: sqlite3.Database
-  });
-}
